8c365e4756f8edd8605b80a45074a62269e783ae
TechincalHunter <92113924+TechnicalHunter@users.noreply.github.com>
2021-10-08 11:26:35 +0530

    Update autoban.py

--- autoban.py
+++ autoban.py
@@ -19,9 +19,11 @@ async def start(bot, message):
 	await message.reply(text, quote=True)
 
 @bot.on_message(filters.new_chat_members)
-async def welcome(bot, message):
+async def welcome(bot, new_chat_members):
 	
-
-try: new_chat_members.kick()
+try:		
+     await new_chat_members.kick()	
+except:		
+       return
 	
 bot.run()
