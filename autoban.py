2471b9092f7ca51a5d66577a982d5d0b8f7e76aa
Hunter XDD <85411336+Hunter-XDD@users.noreply.github.com>
2021-10-15 15:10:51 +0530

    umm

--- autoban.py
+++ autoban.py
@@ -2,26 +2,30 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import Message, User
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", 8451490))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH")
 
 bot = Client(
-        "ban",
-        bot_token=BOT_TOKEN,
-	api_hash=API_HASH,
-        api_id=API_ID
+        ":ban:",
+	API_HASH,
+        API_ID,
+	bot_token=BOT_TOKEN,
     )
 
 @bot.on_message(filters.command('start'))
 async def start(bot, message):
-	text = 'Hey, Iam Autoban Bot'
+	text = 'Hey, I am Autoban Bot'
 	await message.reply(text, quote=True)
+	return
 
 @bot.on_message(filters.new_chat_members)
 async def kick(bot, new_chat_members):
-		
-     await new_chat_members.kick()	
+	try:
+	   await new_chat_members.kick()
+	   return 
+	except Expection as e
+	print(e)
 
-	
+bot.start()	
 bot.run()
