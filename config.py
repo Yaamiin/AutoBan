791335951588352b4ba2c6fe456b01c36c8b5552
Mr.Tanaji <92113924+Mr-Tanaji@users.noreply.github.com>
2021-10-26 21:20:30 +0530

    .

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 from os import getenv
 
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "1437"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
